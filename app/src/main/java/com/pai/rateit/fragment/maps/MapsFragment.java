54e3439c935ee29a3a8b4f6419715838dab7fc1e
kermekx <kermekx@gmail.com>
2018-04-21 15:07:56 +0200

    zoom to location

--- app/src/main/java/com/pai/rateit/fragment/maps/MapsFragment.java
+++ app/src/main/java/com/pai/rateit/fragment/maps/MapsFragment.java
@@ -1,9 +1,17 @@
 package com.pai.rateit.fragment.maps;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,11 +22,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.pai.rateit.R;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 /**
  * Created by kevin on 16/04/2018.
@@ -27,6 +37,7 @@ import butterknife.Unbinder;
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     public static String FRAGMENT_TAG = "MapsFragment";
+    public static int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 411;
 
     SupportMapFragment mapFragment;
     private Unbinder unbinder;
@@ -108,13 +119,71 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            centerMapToCurrentLocation();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                // Add a marker in Sydney and move the camera
+                LatLng lille = new LatLng(51, 3);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(lille));
+                mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                    centerMapToCurrentLocation();
+                }
+            } else {
+                // Permission was denied. Display an error message.
+                // Add a marker in Sydney and move the camera
+                LatLng lille = new LatLng(51, 3);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(lille));
+                mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+            }
+        }
+    }
+
+    @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
+    private void centerMapToCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager)
+                    getActivity().getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+
+
+            Location userPos = locationManager.getLastKnownLocation(locationManager
+                    .getBestProvider(criteria, false));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(userPos.getLatitude(), userPos.getLongitude())));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        }
     }
 
     public interface OnFragmentInteractionListener {
 
     }
+
 }
